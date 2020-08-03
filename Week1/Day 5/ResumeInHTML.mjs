346a39b11f5045d6e94bc520b6467f6936bf168e
VANYA-06 <67746641+VANYA-06@users.noreply.github.com>
2020-08-04 00:09:00 +0530

    Add files via upload

---
+++ Week1/Day 5/ResumeInHTML.mjs
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+h1{text-align: center;}
+h3{text-align: left;}
+body{.color-1 {color: #ffccbc;}
+.color-2 {color: #ffcdd2;}
+.color-3 {color: #ffab91;}
+.color-4 {color: #DAF7A6;}
+.color-5 {color: #FFC300;}
+.color-6 {color: #FF5733;} 
+
+        </style>
+    </head>
+
+
+ <body>
+
+     <h1>RESUME</h1><br>
+
+     <section>
+     
+     <h2>PERSONAL INFO</h2>
+     <style>
+         p{text-align: left;}
+         
+     </style>
+     
+     <p><b><strong>SATYAM R. JAGTAP</strong></b><br>
+            GGD VOICE, Sinhgad Road, <br>                 
+            Vadgaon Bk. Pune, 411041<br>
+            Mobile Number: 7020103900<br>
+            satyam.jagtap1@gmail.com<br>
+            DOB: 25th January 1998<br>
+        </p>
+    </section>    
+<h2>OBJECTIVE</h2>
+
+<p>To utilize my Technical and Management skills for achieving the best 
+         performance in IT Industries.To implement my innovative ideas,
+        skills and creativity for accomplishing new Projects.</p>
+
+<h2>ACADEMIC QUALIFICATION</h2>
+
+<P> <b>DEGREE:-</b> Bachelor Of Engineering.</P>
+<p><b>STREAM:-</b> Computer Engineering.</p>
+<p><b>UNIVERSITY NAME:-</b> Savitribai Phule Pune University.</p>
+<p><b>CGPA:-</b> 8.1</p>
+<p><b>GRADUATION YEAR:-</b> 2020</p><br>
+
+
+<h2>PROJECTS</h2>
+<div><b>1.</b> Project on <b>Library Management System</b> using JAVA.<br>
+   <b> Role Played:</b>Interface Designing, Developer and Database Connectivity.<br> 
+<b>Skills used:</b> Java, NetBeans, MySql, JDBC.<br><br>
+
+<b>2.</b> Project on <b>Automation Testing On Self Made Web Site Using Selenium Tool.</b><br>
+<b>Role Played:</b> Front-end Development, Database Connectivity.<br>
+<b>Skills Used:</b> HTML, CSS, MySql, JDBC.<br><br>
+
+<b>3.</b> Project on <b>Sketch Classification Using CNN Help To Reduce Language Barrier.</b><br>
+<b>Skills Used:</b> Neural Networks (CNN), Deep Learning,Python.<br><br>      
+
+<b>4.</b> Project on<b>Blood Donation Management System</b> Using JAVA.<br>
+<b> Role Played:</b>Interface Designing, Developer and Database Connectivity.<br> 
+<b>Skills used:</b> Java, NetBeans, MySql, JDBC.<br><br>
+
+
+
+<h2>WORKSHOPS & CERTIFICATIONS</h2>
+
+ 
+    <b>1.</b> Participated in Artificial Intelligence & Robotics Workshop.<br>
+    <b>2.</b> Completed the course CORE & Adv. JAVA, Pune. <br>
+    <b>3.</b> Completed Data Structures And Algorithm Certification By Coding Ninjas.<br>
+    <b>4.</b> Completed Python Certification By Coursera.<br>
+    <b>5.</b> Completed JavaScript Certification By GUVI Geek Networks.</div><br><br> 
+
+
+<h2>TECHNICAL SKILLS</h2>
+
+● <b>Database:</b> MySQL,MongoDB.<br>
+● <b>Languages:</b> Java, Object Oriented Concept, Data Structures,JavaScript.<br>
+● <b>Web Technologies:</b> HTML,CSS.<br>
+● <b>Packages used:</b> MS Office (Word, Excel, Power point), Eclipse, NetBeans (8.0.2). <br><br>
+
+
+
+<h2>POSITION OF RESPONSIBLITY</h2>
+
+
+●	House Captain in Kashinath Palod Public School.<br>
+●	Captainship in Cricket For Under 16 (Jain Sports Academy).<br>
+●	Promoting and Preaching About club Activities.<br>
+●	Co-ordinator At Social welfare Club.<br><br>
+
+
+<h2>PERSONALITY TRAITS</h2>
+<p>●	Good communication and presentation Skills.<br>
+    ●	Good Leadership Qualities.<br>
+    ●	Passion for constant Improvement.<br>
+    ●	Willingness to know and apply new things.<br>
+    ●	Well organized and well disciplined.<br>
+    ●	Good Motivator.<br>
+    ●	Ability to work individually as well as in team.<br><br>
+    </p>
+<h2>HOBBIES AND INTEREST</h2>
+<P>
+    ●	Playing Cricket.<br>
+    ●	Singing.<br>
+    ●	Reading Holy Books.<br>
+    ●	Interacting With New People.<br>
+    ●	Swimming in the weekends.<br><br>
+    </P>
+    <h2>DECLARATION</h2>
+    <P>	I hereby declare that the details provided by me in this resume are correct and I have knowingly not omitted/misinterpreted any information.
+   
+    </P>
+</div>
+
+
+</body>
+</html>
